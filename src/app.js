2c5b9717a65207a198d71281c8335f5a37fe1802
Gini <djinymail@yahoo.fr>
2021-08-04 12:04:56 +0200

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "d17c0f5bc809bf15762311858a89abcd";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Reykjavik&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
